197cd295d1fae5a98924975c69012c6c96baabe7
samuelconlon <conlonsp@yahoo.com>
2022-10-16 19:59:32 -0400

    added imports in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,10 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import {Route, Switch} from 'react-router-dom'
+import NavBar from './NavBar';
+import Home from './Home'
+import About from './About';
+import SpotSearch from './SpotSearch'
+import AddSpot from './AddSpot';
 import './App.css';
 
 function App() {
