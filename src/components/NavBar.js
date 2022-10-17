1ae8ae79bd0833971d7e4be1cba7d198ddab540f
samuelconlon <conlonsp@yahoo.com>
2022-10-17 13:14:31 -0400

    added NavLink functionality in Navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,51 @@ const linkStyles = {
 function NavBar() {
 
   return (
-
+    <div className='navbar' style={{textAlign: 'center'}}>
+      <NavLink
+        to='/'
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: 'slategray',
+        }}
+      >
+        Home
+      </NavLink>
+      <span>|</span>
+      <NavLink
+        to='/about'
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: 'slategray',
+        }}
+      >
+        About
+      </NavLink>
+      <span>|</span>
+      <NavLink
+        to='/search'
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: 'slategray',
+        }}
+      >
+        Search
+      </NavLink>
+      <span>|</span>
+      <NavLink
+        to='/add'
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: 'slategray',
+        }}
+      >
+        Add
+      </NavLink>
+    </div>
   )
 }
 
