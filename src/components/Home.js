c26f4bebe59cc9b65e55101162d835b797576598
samuelconlon <conlonsp@yahoo.com>
2022-10-17 13:10:33 -0400

    added JSX to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <div>
-  
+      <h1 style={{textAlign: 'center'}}>Home</h1>
+      <p style={{textAlign: 'center'}}>
+        Welcome to Skate it!  The only place you can go to find the crustiest DIY spots in your area.
+      </p>
     </div>
   )
 }
