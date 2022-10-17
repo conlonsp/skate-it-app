45128be1f0162cbeb585a306b084ee4fcd760386
samuelconlon <conlonsp@yahoo.com>
2022-10-17 13:13:29 -0400

    added style to NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "darkslategray",
+  textDecoration: "none",
+  color: "white",
+};
+
 function NavBar() {
+
   return (
 
   )
