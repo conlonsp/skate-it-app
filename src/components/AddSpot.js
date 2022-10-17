1c4288ecbe06db667715491d754c62bf925b5c4b
samuelconlon <conlonsp@yahoo.com>
2022-10-17 13:21:56 -0400

    added functionality and JSX to AddSpot.js

---
+++ src/components/AddSpot.js
@@ -0,0 +1,111 @@
+import React, {useState} from "react";
+
+function AddSpot({ allSpots, setAllSpots }) {
+  const [addSpot, setAddSpot] = useState({
+    name: '',
+    city: '',
+    state: '',
+    street: '',
+    image: '',
+    description: '',
+  })
+
+  function handleChange(event) {
+    setAddSpot({
+      ...addSpot,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  console.log(addSpot)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const newSpot = {
+      name: addSpot.name,
+      city: addSpot.city,
+      state: addSpot.state,
+      street: addSpot.street,
+      image: addSpot.image,
+      desctiption: addSpot.description,
+    }
+    fetch('http://localhost:3000/spots', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newSpot)
+    })
+    .then(r => r.json())
+    .then(data => setAllSpots([...allSpots, data]))
+    setAddSpot({
+      name: '',
+      city: '',
+      state: '',
+      street: '',
+      image: '',
+      description: '',
+    })
+  }
+
+  // console.log(addSpot)
+
+  return (
+    <div>
+      <h1 style={{textAlign: 'center'}}>Add a Spot</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type='text'
+          name='name'
+          value={addSpot.name}
+          placeholder='name...'
+        />
+        <br/>
+        <input
+          onChange={handleChange}
+          type='text'
+          name='city'
+          value={addSpot.city}
+          placeholder='city...'
+        />
+        <br/>
+        <input
+          onChange={handleChange}
+          type='text'
+          name='state'
+          value={addSpot.state}
+          placeholder='state...'
+        />
+        <br/>
+        <input
+          onChange={handleChange}
+          type='text'
+          name='street'
+          value={addSpot.street}
+          placeholder='street...'
+        />
+        <br/>
+        <input
+          onChange={handleChange}
+          type='text'
+          name='image'
+          value={addSpot.image}
+          placeholder='image URL...'
+        />
+        <br/>
+        <textarea
+          onChange={handleChange}
+          type='text'
+          name='description'
+          value={addSpot.description}
+          placeholder='description...'
+        />
+        <br/>
+        <button>Submit Spot!</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddSpot
