8303f2a0dd3d10f91f6cf0b6d2e4d8dba8a5266b
samuelconlon <conlonsp@yahoo.com>
2022-10-17 16:26:00 -0400

    added Grid, Card, Imageto Spot.js

--- src/components/Spot.js
+++ src/components/Spot.js
@@ -1,12 +1,34 @@
 import React from "react";
-import {Card} from 'semantic-ui-react'
+import {Card, Grid, Image} from 'semantic-ui-react'
 
 function Spot({ spot }) {
   console.log(spot)
   return (
-    <Card id='spot-card'>
-      hello!
-    </Card>
+    <Grid>
+      <Grid.Column>
+        <Image
+          src={spot.image}
+          floated="left"
+          size="small"
+          className="hostImg"
+        />
+      </Grid.Column>
+      <Grid.Column>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              {spot.name}
+            </Card.Header>
+            <Card.Description>
+              {spot.city}, {spot.state}
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Grid.Column>
+    </Grid>
+    // <Card id='spot-card'>
+    //   hello!
+    // </Card>
   )
 }
 
