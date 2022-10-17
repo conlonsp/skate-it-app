836b8fe05751736a715a9e00c8819dcda925f0e7
samuelconlon <conlonsp@yahoo.com>
2022-10-17 13:11:28 -0400

    created About component in About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1 style={{textAlign: 'center'}}>About</h1>
+      <p style={{textAlign: 'center'}}>This app is made using React!</p>
+    </div>
+  )
+}
+
+export default About
