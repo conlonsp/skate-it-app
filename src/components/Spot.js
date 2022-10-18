ef0bdd715489df6145a00e0a5c4d5b6420ccd4c4
samuelconlon <conlonsp@yahoo.com>
2022-10-18 19:53:21 -0400

    updated styling in App.css and Spot.js

--- src/components/Spot.js
+++ src/components/Spot.js
@@ -4,39 +4,24 @@ import {Card, Grid, Image} from 'semantic-ui-react'
 function Spot({ spot }) {
   console.log(spot)
   return (
-    <Grid id='spot-grid'>
-      {/* <Grid.Column>
+    <Card id='spot-card'>
+      <Card.Content>
+        <Card.Header>
+          {spot.name}
+        </Card.Header>
+        <Card.Description>
+          {spot.street}
+          <br/>
+          {spot.city}, {spot.state}
+        </Card.Description>
+        <br/>
         <Image
           src={spot.image}
-          floated="left"
-          
-          className="spotImg"
+          floated='left'
+          size='medium'
         />
-      </Grid.Column> */}
-      <Grid.Column>
-        <Card>
-          <Card.Content>
-            <Card.Header>
-              {spot.name}
-            </Card.Header>
-            <Card.Description>
-              {spot.street}
-              <br/>
-              {spot.city}, {spot.state}
-            </Card.Description>
-            <Image
-              src={spot.image}
-              floated='left'
-              size='medium'
-              //className='spotImg'
-            />
-          </Card.Content>
-        </Card>
-      </Grid.Column>
-    </Grid>
-    // <Card id='spot-card'>
-    //   hello!
-    // </Card>
+      </Card.Content>
+    </Card>
   )
 }
 
