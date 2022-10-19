c330892f26228d559655c0a1bfdb79ca1788294a
samuelconlon <conlonsp@yahoo.com>
2022-10-19 15:11:09 -0400

    updated text inside About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,9 @@ function About() {
     <div>
       <h1 style={{textAlign: 'center'}}>About</h1>
       <p style={{textAlign: 'center'}}>This app is made using React!</p>
+      <br/>
+      <p style={{textAlign: 'center'}}>How To Use:</p>
+      <p style={{textAlign: 'center'}}>Look for DIY spots in your area by typing in the city or town name.  A list of DIY spots in that area will appear on the page.  Click on whichever spot interests you more to learn more about it!  Happy Skating!</p>
     </div>
   )
 }
