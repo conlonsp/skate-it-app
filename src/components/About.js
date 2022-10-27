0861f9cb54fdb77debcefaea8575d864ce929e32
samuelconlon <conlonsp@yahoo.com>
2022-10-26 22:43:11 -0400

    added text to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,29 @@ function About() {
     <div style={{textAlign: 'center'}}>
       <br/>
       <h1>About</h1>
-      <p style={{fontSize: '20px'}}>This app is made using React!</p>
+      <p style={{fontSize: '20px'}}>
+        This app is made using React!
+      </p>
+      <p style={{fontSize: '20px'}}>
+        For skateboarders, by skateboarders.
+      </p>
+      <p style={{fontSize: '20px'}}>
+        My love for DIY skate spots inspired me to make an app in the hopes of satisfying your thirst for these homemade spots!
+      </p>
       <br/>
       <p>How To Use:</p>
-      <p style={{fontSize: '20px'}}>Look for DIY spots in your area by typing in the city or town name.</p>
-      <p style={{fontSize: '20px'}}>A list of DIY spots in that area will appear on the page.</p>
-      <p style={{fontSize: '20px'}}>Click on whichever spot interests you most to learn more about it!</p>
-      <p style={{fontSize: '20px'}}>Happy Skating!</p>
+      <p style={{fontSize: '20px'}}>
+        Look for DIY spots in your area by typing in the city or town name
+      </p>
+      <p style={{fontSize: '20px'}}>
+        A list of DIY spots in that area will appear on the page.
+      </p>
+      <p style={{fontSize: '20px'}}>
+        Click on whichever spot interests you most to learn more about it!
+      </p>
+      <p style={{fontSize: '20px'}}>
+        Happy Skating!
+      </p>
     </div>
   )
 }
