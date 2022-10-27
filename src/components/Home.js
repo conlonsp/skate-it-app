be4c0c1e98c17619e9735d36d192a4c34ff6377f
samuelconlon <conlonsp@yahoo.com>
2022-10-26 22:28:58 -0400

    updated text in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,25 @@ function Home() {
   return (
     <div>
       <h1 style={{textAlign: 'center'}}>Home</h1>
+      <p style={{textAlign: 'center', fontSize: '25px'}}>
+        Welcome to Skate it!
+      </p>
       <p style={{textAlign: 'center', fontSize: '20px'}}>
-        Welcome to Skate it!  The only place you can go to find the crustiest DIY spots in your area.
+        The only place you can go to find the crustiest DIY spots in your area.
+      </p>
+      <br/>
+      <br/>
+      <p style={{textAlign: 'center', fontSize: '20px'}}>
+        Click on the tabs in the navigation bar to explore the app further!
+      </p>
+      <p style={{textAlign: 'center', fontSize: '20px'}}>
+        The About page will tell you more about this app and how to use it.
+      </p>
+      <p style={{textAlign: 'center', fontSize: '20px'}}>
+        The Search page will allow you to look up DIY spots in your area.
+      </p>
+      <p style={{textAlign: 'center', fontSize: '20px'}}>
+        And finally, the Add page will allow you to add spots to our growing database!
       </p>
     </div>
   )
