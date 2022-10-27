a0675f92ef71ae8a1f6bfda0ef8994d37f5194de
samuelconlon <conlonsp@yahoo.com>
2022-10-26 22:31:55 -0400

    updated styling in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,27 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      <h1 style={{textAlign: 'center'}}>Home</h1>
-      <p style={{textAlign: 'center', fontSize: '25px'}}>
+    <div style={{textAlign: 'center'}}>
+      <br/>
+      <h1>Home</h1>
+      <p style={{fontSize: '25px'}}>
         Welcome to Skate it!
       </p>
-      <p style={{textAlign: 'center', fontSize: '20px'}}>
+      <p style={{fontSize: '20px'}}>
         The only place you can go to find the crustiest DIY spots in your area.
       </p>
       <br/>
       <br/>
-      <p style={{textAlign: 'center', fontSize: '20px'}}>
+      <p style={{fontSize: '20px'}}>
         Click on the tabs in the navigation bar to explore the app further!
       </p>
-      <p style={{textAlign: 'center', fontSize: '20px'}}>
+      <p style={{fontSize: '20px'}}>
         The About page will tell you more about this app and how to use it.
       </p>
-      <p style={{textAlign: 'center', fontSize: '20px'}}>
+      <p style={{fontSize: '20px'}}>
         The Search page will allow you to look up DIY spots in your area.
       </p>
-      <p style={{textAlign: 'center', fontSize: '20px'}}>
+      <p style={{fontSize: '20px'}}>
         And finally, the Add page will allow you to add spots to our growing database!
       </p>
     </div>
