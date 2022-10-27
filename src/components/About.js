7a4c7e86b948c128d7aa11f49105e9fecd6c590d
samuelconlon <conlonsp@yahoo.com>
2022-10-26 22:30:19 -0400

    updated syling in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ function About() {
   return (
     <div style={{textAlign: 'center'}}>
       <br/>
-      <h1 style={{textAlign: 'center'}}>About</h1>
-      <p style={{textAlign: 'center', fontSize: '20px'}}>This app is made using React!</p>
+      <h1>About</h1>
+      <p style={{fontSize: '20px'}}>This app is made using React!</p>
       <br/>
-      <p style={{textAlign: 'center'}}>How To Use:</p>
+      <p>How To Use:</p>
       <p style={{fontSize: '20px'}}>Look for DIY spots in your area by typing in the city or town name.</p>
       <p style={{fontSize: '20px'}}>A list of DIY spots in that area will appear on the page.</p>
       <p style={{fontSize: '20px'}}>Click on whichever spot interests you most to learn more about it!</p>
